ed1f9cf7add43078ccfee27231d41b244579f3fa
tanayshandilya <tanay.shandilya99@gmail.com>
2023-11-12 12:21:41 +0530

    CORS for UI Host updated

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.get('*', (req, res) => {
 const server = http.createServer(app)
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
     methods: ['GET', 'POST'],
   },
 })
